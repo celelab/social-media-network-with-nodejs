0f1f974e76abc5b05a5c2bdfa7bde15cb7ae39ab
gabriedev <gabriedev@gmail.com>
2023-03-10 13:18:49 -0400

    🎉 Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Setup
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Static files
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Listener
+app.listen(app.get('port'), () => {
+    console.log('Server is running on port', app.get('port'))
+})
